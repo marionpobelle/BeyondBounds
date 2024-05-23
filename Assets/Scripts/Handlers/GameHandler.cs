0e0d1fcd0a71b16385b4fcbef82588f15b6410e7
Marion Pobelle <marion.pobelle@hotmail.com>
2024-05-23 18:36:03 +0200

    Depleting resources system

--- Assets/Scripts/Handlers/GameHandler.cs
+++ Assets/Scripts/Handlers/GameHandler.cs
@@ -11,6 +11,8 @@ public class GameHandler : MonoBehaviour
     [SerializeField, Tooltip("Current game run time.")] public float TotalRunTime = 0.0f;
 
     [SerializeField, Tooltip("Global resources. 0:WATER, 1:FOOD, 2:ENERGY, 3:HEALTH")] private Vector4 _globalResources;
+    [SerializeField, Tooltip("By how much the global resources deplete when they do. 0:WATER, 1:FOOD, 2:ENERGY, 3:HEALTH")] private Vector4 _globalResourcesDepletingAmounts;
+    [SerializeField, Tooltip("Time in seconds in between each resource depleting. 0:WATER, 1:FOOD, 2:ENERGY, 3:HEALTH")] private Vector4 _globalResourcesDepletingCooldowns;
 
     [SerializeField] private TextMeshProUGUI _waterText;
     [SerializeField] private TextMeshProUGUI _foodText;
@@ -29,7 +31,8 @@ public class GameHandler : MonoBehaviour
     //Start is called on the frame when a script is enabled
     private void Start()
     {
-        UpdateHUDResources(_globalResources);
+        UpdateHUDResources();
+        DepleteGlobalResources();
     }
 
     // Update is called once per frame
@@ -61,19 +64,82 @@ public class GameHandler : MonoBehaviour
     private void OnResourceChange(Vector4 resourceChanges)
     {
         _globalResources += resourceChanges;
-        UpdateHUDResources(resourceChanges);
+        UpdateHUDResources();
     }
 
     /// <summary>
     /// Updates the HUD resource values.
     /// /// <param name="resourceChanges">Changs to apply.</param>
     /// </summary>
-    private void UpdateHUDResources(Vector4 resourceChanges)
+    private void UpdateHUDResources()
     {
-        _waterText.text = resourceChanges.x.ToString();
-        _foodText.text = resourceChanges.y.ToString();
-        _energyText.text = resourceChanges.z.ToString();
-        _healthText.text = resourceChanges.w.ToString();
+        _waterText.text = _globalResources.x.ToString();
+        _foodText.text = _globalResources.y.ToString();
+        _energyText.text = _globalResources.z.ToString();
+        _healthText.text = _globalResources.w.ToString();
+    }
+
+    /// <summary>
+    /// Coroutine that depletes the water resource over time
+    /// </summary>
+    private IEnumerator DepleteResourceWater()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(_globalResourcesDepletingCooldowns[0]);
+            _globalResources[0] -= _globalResourcesDepletingAmounts[0];
+            UpdateHUDResources();
+        }
+    }
+
+    /// <summary>
+    /// Coroutine that depletes the food resource over time
+    /// </summary>
+    private IEnumerator DepleteResourceFood()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(_globalResourcesDepletingCooldowns[1]);
+            _globalResources[1] -= _globalResourcesDepletingAmounts[1];
+            UpdateHUDResources();
+        }
+    }
+
+    /// <summary>
+    /// Coroutine that depletes the energy resource over time
+    /// </summary>
+    private IEnumerator DepleteResourceEnergy()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(_globalResourcesDepletingCooldowns[2]);
+            _globalResources[2] -= _globalResourcesDepletingAmounts[2];
+            UpdateHUDResources();
+        }
+    }
+
+    /// <summary>
+    /// Coroutine that depletes the health resource over time
+    /// </summary>
+    private IEnumerator DepleteResourceHealth()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(_globalResourcesDepletingCooldowns[3]);
+            _globalResources[3] -= _globalResourcesDepletingAmounts[3];
+            UpdateHUDResources();
+        }
+    }
+
+    /// <summary>
+    /// Depletes all resources over time.
+    /// </summary>
+    private void DepleteGlobalResources()
+    {
+        StartCoroutine(DepleteResourceWater());
+        StartCoroutine(DepleteResourceFood());
+        StartCoroutine(DepleteResourceEnergy());
+        StartCoroutine(DepleteResourceHealth());
     }
 
     //OnDestroy is called when Destroying the attached Behaviour. It will only be called on game objects that have previously been active.
